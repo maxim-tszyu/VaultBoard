3b5405e82fa4c99c1a82931695b994d33eadcc47
maxim_tszyu <tszyuoa@gmail.com>
2025-08-09 21:50:19 +0500

    installed breeze, laid down basic layup for the project

---
+++ app/Http/Controllers/FinanceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Finance;
+use App\Http\Requests\StoreFinanceRequest;
+use App\Http\Requests\UpdateFinanceRequest;
+
+class FinanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFinanceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Finance $finance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Finance $finance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFinanceRequest $request, Finance $finance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Finance $finance)
+    {
+        //
+    }
+}
