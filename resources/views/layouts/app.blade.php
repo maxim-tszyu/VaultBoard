95d082ac83f5714d9d74b8e0b1ce35af0f79994b
maxim_tszyu <tszyuoa@gmail.com>
2025-08-13 13:23:11 +0500

    laid down basic design, to be improved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.bunny.net">
+	<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen flex">
+	<!-- Header -->
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+	<aside class="w-64 h-screen bg-gray-50 dark:bg-gray-800">
+		@include('layouts.sidebar')
+	</aside>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+	<div class="flex-1 flex flex-col">
+
+		<div class="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 h-16 flex items-center">
+			@include('layouts.navigation')
+		</div>
+
+		<main class="flex-1 overflow-auto">
+			{{ $slot }}
+		</main>
+	</div>
+</div>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,40 @@
 <?php
 
+use App\Http\Controllers\AnalysisController;
+use App\Http\Controllers\EntryController;
+use App\Http\Controllers\FinanceController;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+	return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::prefix('/tasks')->name('tasks.')->group(function () {
+		Route::get('/', [TaskController::class, 'index'])->name('index');
+	});
+	Route::prefix('/notes')->name('notes.')->group(function () {
+		Route::get('/', [NoteController::class, 'index'])->name('index');
+	});
+	Route::prefix('/entries')->name('entries.')->group(function () {
+		Route::get('/', [EntryController::class, 'index'])->name('index');
+	});
+	Route::prefix('/finances')->name('finances.')->group(function () {
+		Route::get('/', [FinanceController::class, 'index'])->name('index');
+	});
+	Route::get('/analysis', [AnalysisController::class])->name('analysis');
+	Route::get('/report', [ReportController::class])->name('report');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
